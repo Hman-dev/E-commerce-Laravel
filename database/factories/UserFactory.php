71ca696294516b71b4c50f6b2bceca302f801dcd
Hman-dev <hermannflori@gmail.com>
2020-12-18 23:20:15 +0100

    Création user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+// use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +25,9 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'newsletter' => $faker->boolean(),
+        'last_seen' => $faker->dateTimeBetween('-6 months'),
+        'admin' => false,
+        'created_at' => $faker->dateTimeBetween('-4 years', '-6 months'),
     ];
 });
